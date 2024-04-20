348eefe19f4ce5bc9bd34e2d22461bfc67800f6f
muthukrishnan25 <muthukrishnan121317@gmail.com>
2024-04-20 13:11:09 +0530

    required fields

---
+++ src/main/java/erp/javaguides/erpbackend/CorsConfig.java
@@ -0,0 +1,25 @@
+package erp.javaguides.erpbackend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Allow requests from this origin
+                .allowedMethods("*") // Allow all HTTP methods
+                .allowedHeaders("*"); // Allow all headers
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+}
