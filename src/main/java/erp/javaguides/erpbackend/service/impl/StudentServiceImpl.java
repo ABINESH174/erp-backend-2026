21cb84227af283fc9b88700d57c1052a3ac5e9d9
VijayMuruga12 <vijaymuruga200201@gmail.com>
2024-09-04 10:20:03 +0530

    File local storage

--- src/main/java/erp/javaguides/erpbackend/service/impl/StudentServiceImpl.java
+++ src/main/java/erp/javaguides/erpbackend/service/impl/StudentServiceImpl.java
@@ -178,7 +178,13 @@ public class StudentServiceImpl implements StudentService {
             return studentWithFilesDto;
         }).collect(Collectors.toList());
     }
-
+    @Override
+    public List<StudentWithFilesDto> getAllStudentsByDiscipline(String discipline) {
+        List<Student> cseStudents = studentRepository.findByDiscipline(discipline);
+        return cseStudents.stream()
+                .map(StudentMapper::mapToStudentWithFilesDto)
+                .collect(Collectors.toList());
+    }
     public MultipartFile base64ToMultipartFile(String base64, String fileName) {
         try {
             byte[] fileBytes;
