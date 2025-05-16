93c5f38e96e0e06d5bc94ce48def4df755eeab1f
Abinesh S A <abivinuahila@gmail.com>
2025-05-16 19:02:54 +0530

    added principal and office bearer functionalities

--- src/main/java/erp/javaguides/erpbackend/controller/OfficeBearerController.java
+++ src/main/java/erp/javaguides/erpbackend/controller/OfficeBearerController.java
@@ -1,11 +1,21 @@
 package erp.javaguides.erpbackend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import erp.javaguides.erpbackend.service.OfficeBearerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 @RequestMapping("/api/office-bearer")
 public class OfficeBearerController {
     
+    @Autowired
+    private OfficeBearerService officeBearerService;
+    
+
 }
