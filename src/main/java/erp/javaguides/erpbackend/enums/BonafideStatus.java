628275c85b5fc2c168fe3a57efdcf5973ac1466e
Abinesh S A <abivinuahila@gmail.com>
2025-06-24 12:04:25 +0530

    Added facultyRejected and hodRejected status and some minor git changes in properties file

--- src/main/java/erp/javaguides/erpbackend/enums/BonafideStatus.java
+++ src/main/java/erp/javaguides/erpbackend/enums/BonafideStatus.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum BonafideStatus {
-    PENDING,FACULTY_APPROVED,HOD_APPROVED,OB_APPROVED,PRINCIPAL_APPROVED,REJECTED,OB_REJECTED,NOTIFIED;
+    PENDING,FACULTY_APPROVED,HOD_APPROVED,OB_APPROVED,PRINCIPAL_APPROVED,REJECTED,OB_REJECTED,FACULTY_REJECTED,HOD_REJECTED,NOTIFIED;
 
     @JsonCreator
     public static BonafideStatus fromValue(String value){
